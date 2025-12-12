0e7cd5c9380d083fd84965433ead03c068fefcae
rashasharma <sharmarasha5@gmail.com>
2025-12-12 15:43:56 +0530

    better matches

--- model/recommender.py
+++ model/recommender.py
@@ -12,23 +12,36 @@ print(f" Data Loaded. Total Rows: {len(df)}")
 
 print(" Building Vector Matrix...")
 vectorizer = TfidfVectorizer(stop_words='english',min_df=2)
-tfdif_matrix = vectorizer.fit_transform(df['Notes'].fillna(''))
+tfidf_matrix = vectorizer.fit_transform(df['Notes'].fillna(''))
 
-def get_recommendations(perfume_name, top_n=5):
-    mask = df['Name'].str.contains(perfume_name, case=False, na=False) | \
-           df['Brand'].str.contains(perfume_name, case=False, na=False)
+def get_recommendations(user_input, top_n=5):
+    if not user_input:
+        return["Error no input."]
+    query = str(user_input).strip()
+    mask = df['Name'].str.contains(query, case=False, na=False) | \
+           df['Brand'].str.contains(query, case=False, na=False)
     
     matches = df[mask]
+    target_vector=None
+    search_type="perfume"
 
-    if matches.empty:
-        return [f"Error: Perfume '{perfume_name}' not found."]
-    idx = matches.index[0]
-    target_vector=tfdif_matrix[idx]
-    cosine_scores=linear_kernel(target_vector,tfdif_matrix).flatten()
-
+    if not matches.empty:
+        idx = matches.index[0]
+        target_vector=tfidf_matrix[idx]
+        print(f"MATCHED PERFUME:{df.iloc[idx]['Name']}")
+    
+    else:
+        print(f"treating '{query}'as raw ingredient")
+        search_type="ingredient"
+        target_vector=vectorizer.transform([query])
+         
+        if target_vector.nnz==0:
+            return[f"Error: The word '{query}' isn't in our scent vocabulary."]
+    cosine_scores=linear_kernel(target_vector,tfidf_matrix).flatten()
     sim_scores = list(enumerate(cosine_scores))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:top_n+1]
+    start_index=1 if search_type=="perfume" else 0
+    sim_scores = sim_scores[start_index:top_n+start_index]
     
     perfume_indices = [i[0] for i in sim_scores]
     return df[['Name', 'Brand', 'Image_URL']].iloc[perfume_indices].values.tolist()
